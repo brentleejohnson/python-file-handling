e82fb7f035188c4a3987a207cb77fc361659960f
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-24 16:26:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# f = open("yamkela.txt", "w")
+# f.write("You are so funny \n")
+# f.write("You wanted Jason to do the work for you \n")
+# f.write("You knew that I was going to call your name \n")
+# f.close()
+
+# g = open("yamkela.txt", "r")
+# for x in g:
+#     print(x, end="")
+
+with open("runningaway.txt", "w") as myfile:
+    myfile.write("Going out")
+    myfile.write("delaying tactic")
